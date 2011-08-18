cb59564f02bab996b9d6dc5b8dc5721329c05da9
Naitik Shah <n@daaku.org>
2011-08-17 18:08:57 -0700

    issued_at should be an int

--- test/signed-request-test.js
+++ test/signed-request-test.js
@@ -1,10 +1,22 @@
 var signedRequest = require('signed-request')
   , assert = require('assert')
 
+function isInt(n) {
+  return typeof n == 'number' && n % 1 === 0
+}
+
 exports['sign and verify'] = function(beforeExit) {
   var data = { answer: 42 }
     , secret = 'abcd'
     , signed = signedRequest.stringify(data, secret)
     , parsed = signedRequest.parse(signed, secret, 1000)
   assert.equal(data.answer, parsed.answer, 'expect the answer.')
+}
+
+exports['issued_at is an integer'] = function(beforeExit) {
+  var data = { answer: 42 }
+    , secret = 'abcd'
+    , signed = signedRequest.stringify(data, secret)
+    , parsed = signedRequest.parse(signed, secret, 1000)
+  assert.ok(isInt(parsed.issued_at))
 }
